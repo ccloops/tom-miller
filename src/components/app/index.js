1aa0edd409d984554ff267af7baddb5b80facf81
Catherine Looper <loopercat@gmail.com>
2018-08-07 18:53:04 -0700

    added home page components

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,13 +1,25 @@
 import React, { Component, Fragment } from 'react';
 
-import NavLogo from '../../assets/logo-one.jpeg';
+import NavLogo from '../../assets/header-image-bw.png';
+import RainbowStructure from '../../assets/utep-driveby.jpg';
+
+import '../../style/reset.scss';
+import './app.scss';
 
 export default class App extends Component {
   render() {
     return (
       <Fragment>
-        <h1>Hello Tom</h1>
-        <img src={NavLogo} alt="" />
+        <header>
+          <img src={NavLogo} alt='Tom is a tringle' />
+        </header>
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Drawings</li>
+          <li>About</li>
+        </ul>
+        <img id='rainbow' src={RainbowStructure} alt='rainbow structure' />
       </Fragment>
     );
   }
