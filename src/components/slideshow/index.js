d159977ed99dc7175a31476f5f50af1043ad041f
Catherine Looper <loopercat@gmail.com>
2018-09-15 11:02:04 -0700

    added responsive carousel styling

--- src/components/slideshow/index.js
+++ src/components/slideshow/index.js
@@ -1,5 +1,8 @@
 import React, { Component, Fragment } from 'react';
 
+import 'style-loader!css-loader!react-responsive-carousel/lib/styles/carousel.css';
+
+
 import { Carousel } from 'react-responsive-carousel';
 
 import RainbowStructure from '../../assets/utep-driveby.jpg';
@@ -9,18 +12,15 @@ import Chandelier from '../../assets/suncadia-inn-chandelier.jpeg';
 export default class SlideShow extends Component {
   render() {
     return(
-      <Carousel>
+      <Carousel showArrows={ true }>
         <div>
           <img src={ RainbowStructure } />
-          <p className='legend'>Legend 1</p>
         </div>
         <div>
           <img src={ Table } />
-          <p className='legend'>Legend 2</p>
         </div>
         <div>
           <img src={ Chandelier } />
-          <p className='legend'>Legend 3</p>
         </div>
       </Carousel>
     );
